39f0e8c026f07ce24a2b539787360ee8fa2d29a6
Anil Varma <varma.rymec@gmail.com>
2023-01-27 08:38:02 +0530

    qurey

--- expressjs/07-params-query.js
+++ expressjs/07-params-query.js
@@ -5,39 +5,97 @@ const app = express()
 const { products, people } = require('./data')
 
 // Creating api route
-app.get('/', (req, res) => {
-    res.send('<h1><a href="/api/v1/products">Home Page</a></h1>')
-})
+// app.get('/', (req, res) => {
+//     res.send('<h1><a href="/api/v1/products">Home Page</a></h1>')
+// })
+//===============  PARAMS  ======================
+
+// app.get('/api/v1/products', (req, res) => {
+
+//     const newProducts = products.map((product) => {
+//         const { id, name, image } = product
+//         return { id, name, image }
+//     })
+
+//     res.status(200).json(newProducts)
+//     res.send(products)
+// })
+
+// app.get('/api/v1/products/:productId', (req, res) => {
+//     const { productId } = req.params
+
+//     const singleProduct = products.find((product) => product.id === Number(productId))
+//     // console.log('productId', productId) 
+
+//     if (!singleProduct) {
+//         return res.status(404).send("product not found")
+//     }
+//     else {
+//         return res.json(singleProduct)
+//     }
+// })
+
+// app.get('/api/products/:productId/reviews/:reviewId', (req, res) => {
+//     console.log(req.params)
+//     res.send('Hello Products')
+// })
+
+//===============  QUERY  ======================
+
+
+// app.get('/api/v1/query', (req, res) => {
+//     // console.log('This is the query', req.query)
+//     // res.send('Query sent') 
+//     const { search, limit } = req.query
+//     let sortedProducts = [...products]
+
+//     if (search) {
+//         sortedProducts = sortedProducts.filter((product) => {
+//             return product.name.startsWith(search)
+//         })
+//         // res.send(sortedProducts) 
+
+//     }
+
+//     if (limit) {
+//         sortedProducts = sortedProducts.slice(0, Number(limit))
+//     }
+
+//     if (sortedProducts.length < 1) {
+//         res.status(200).send('No products found!!!')
+//         // return res.status(200).json({ success: true, data: [] }) 
+//     }
+
+//     res.status(200).json(sortedProducts)
+
+// })
+
 
 app.get('/api/v1/products', (req, res) => {
 
-    const newProducts = products.map((product) => {
-        const { id, name, image } = product
-        return { id, name, image }
-    })
+    const { search, limit } = req.query
+    let sortedData = [...products]
 
-    res.status(200).json(newProducts)
-    // res.send(products)
-})
 
-app.get('/api/v1/products/:productId', (req, res) => {
-    const { productId } = req.params
-
-    const singleProduct = products.find((product) => product.id === Number(productId))
-    // console.log('productId', productId) 
-
-    if (!singleProduct) {
-        return res.status(404).send("product not found")
+    if (search) {
+        sortedData = sortedData.filter((product) => {
+            return product.name.startsWith(search)
+        })
     }
-    else {
-        return res.json(singleProduct)
+
+    if (limit) {
+        sortedData = sortedData.slice(0, Number(limit))
     }
+
+    if (sortedData.length < 1) {
+        return res.status(200).send("There are no products with your search")
+    }
+
+    res.status(200).json(sortedData)
+
 })
 
-app.get('/api/products/:productId/reviews/:reviewId', (req, res) => {
-    console.log(req.params)
-    res.send('Hello Products')
-})
+
 
 app.listen(8800, () => {
     console.log('Server started at port 8800...')
